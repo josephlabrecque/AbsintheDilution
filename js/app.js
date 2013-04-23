073b13b5dfaba7651e501711aab7a3f11b5fa756
Joseph Labrecque <Joseph.Labrecque@du.edu>
2013-04-23 07:33:45 -0600

    2.0.20

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function onDeviceReady() {
     
 $(document).bind("mobileinit", function () {
     //$.mobile.pageContainer = $('#container');
-    $.mobile.defaultPageTransition = "none";
+    $.mobile.defaultPageTransition = "fade";
     
     $.support.cors = true;
     $.mobile.touchOverflowEnabled = true;
@@ -40,7 +40,7 @@ $(document).bind("mobileinit", function () {
     $.mobile.pushStateEnabled = false;
 
     $.extend($.mobile, {
-        defaultPageTransition: "none"
+        defaultPageTransition: "fade"
     });
 });
 
